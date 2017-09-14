9a60c2d8762c1eb93d0da72ac1d1f7cf32ea09f1
mich282q <31951247+mich282q@users.noreply.github.com>
2017-09-14 13:09:02 +0200

    Add files via upload

--- Welcome.java
+++ Welcome.java
@@ -1,5 +1,8 @@
 public class Welcome {
 	public static void main(String[] args) {
 		System.out.println("Welcome to java!");
+		System.out.println("Hej dumme!");
+		System.out.println("hej smukke!");
+		System.out.println("hej du er sjov!");
 	}
 }
